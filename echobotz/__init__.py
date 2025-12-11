a23f85d8ca6f18a43cd94a2cd0487efa4cc1c9c0
TheZake <patrioremrej@gmail.com>
2025-12-11 17:47:31 +0530

    1

--- echobotz/__init__.py
+++ echobotz/__init__.py
@@ -1,1 +1,37 @@
+import asyncio
+from uvloop import install
+from logging import (
+    ERROR,
+    INFO,
+    WARNING,
+    FileHandler,
+    StreamHandler,
+    basicConfig,
+    getLogger,
+)
 
+from config import Config
+
+basicConfig(
+    format="[%(asctime)s] [%(levelname)s] - %(message)s",
+    datefmt="%d-%b-%y %I:%M:%S %p",
+    handlers=[FileHandler("log.txt"), StreamHandler()],
+    level=INFO,
+)
+
+getLogger("pyrogram").setLevel(ERROR)
+getLogger("pymongo").setLevel(WARNING)
+
+LOGGER = getLogger(__name__)
+
+install()
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+user_data = {}
+auth_chats = {}
+sudo_users = set(Config.SUDO_USERS)
---
+++ echobotz/eco.py
@@ -0,0 +1,52 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+class echo(object):
+    ABC = """<b><u>POSTER SCRAPPER BOT</u></b>
+
+<b>This bot scrapes posters from various OTT platforms and bypasses direct links from cloud sites.</b>
+
+<b>✺ Commands</b>
+<blockquote expandable>
+/poster - Scrape any movie/show poster
+/overlap - Overlay a logo on a poster
+/imdb - Search movie/series on IMDb
+/anime - Search Anime on Anilist
+/gdflix - Bypass GDFlix links
+/hubcloud - Bypass HubCloud links
+/hubdrive - Bypass Hubdrive links
+/transfer_it - Bypass Transfer.it links
+/prime - Prime Video poster
+/zee5 - ZEE5 poster
+/appletv - Apple TV+ poster
+/airtel - Airtel Xstream poster
+/sunnxt - Sun NXT poster
+/aha - Aha Video poster
+/iqiyi - iQIYI poster
+/wetv - WeTV poster
+/shemaroo - ShemarooMe poster
+/bms - BookMyShow poster
+/plex - Plex TV poster
+/adda - Addatimes poster
+/stage - Stage poster
+/netflix - Netflix poster
+/youtube - YouTube thumbnail
+/instagram - Instagram thumbnail
+/facebook - Facebook thumbnail
+/tiktok - TikTok thumbnail
+</blockquote>
+
+<b>Examples</b>
+<code>/poster Avatar</code>
+<code>/poster Avatar 2022</code>
+<code>/poster Avatar: The Way of Water</code>
+
+<b>NOTE:</b> Bot can filter results by keywords and release year.
+"""
+    IMG = "https://i.ibb.co/ymD61smt/photo-2025-12-10-13-50-26-7582226305333067796.jpg"
+    EFCT = 5104841245755180586 # 🔥 Fire Effect Animation, not using bcz working only in bot pm
+    ST_BTN = "Repo"
+    REPO = "https://github.com/XalFH/Poster-Scraper-Bot"
+    UP_BTN = "Updates"
+    UPDTE = "https://t.me/EchoBotz"
+    SP_BTN = "Support Group"
+    SP_GR = "https://t.me/NxTalks"
